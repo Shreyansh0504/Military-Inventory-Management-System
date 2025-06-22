0e5e13e1959d35cad38cb2e684a6c525bd16b495
Shreyansh Tripathi <shreyansh.tripathi0503@gmail.com>
2025-06-22 19:07:46 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
-import cors from 'cors';
 import morgan from 'morgan';
 import { conn } from './config/db.js';
 import { userRouter } from './routes/userRoutes.js';
@@ -13,25 +12,37 @@ import { expenseRouter } from './routes/expenditureRoutes.js';
 import { transferRouter } from './routes/transferRoutes.js';
 
 const app = express();
-app.use(express.json());
-app.use(morgan("dev"));
 dotenv.config();
 
 // Database connection
 conn();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename); 
+
+
+import cors from 'cors';
+
 // Enhanced CORS configuration
 app.use(
   cors({
     origin: "https://shreyansh-military-ims.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+    
   })
 );
 
+
+    // methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+// allowedHeaders: ["Content-Type", "Authorization"],
+    // credentials: true,
+
+app.use(express.json())
+app.use(morgan('dev'))
+
 // Explicit OPTIONS handler
-app.options('*', cors());
+// app.options('*', cors());
 
 // Routes
 app.use('/api/v1/user', userRouter);
@@ -43,12 +54,17 @@ app.use("/api/v1/expense", expenseRouter);
 app.use("/api/v1/transfer", transferRouter);
 
 // Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Server error');
-});
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).send('Server error');
+// });
 
 const PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`.bgGreen);
