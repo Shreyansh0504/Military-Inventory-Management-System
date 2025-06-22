526be8d6c8fe9f1a627d405e887239a2ccac7dd2
Shreyansh Tripathi <shreyansh.tripathi0503@gmail.com>
2025-06-22 19:27:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -63,10 +63,21 @@ app.use("/api/v1/transfer", transferRouter);
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
-});
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+// });
+
+
+// Serve static frontend (only in production)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  });
+}
+
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`.bgGreen);
